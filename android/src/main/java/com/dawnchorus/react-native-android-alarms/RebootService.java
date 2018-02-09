76a89a79abd49ec4c95cbbeee865348144e568a7
Ticknor, Samantha <TicknorS@CarnegieMuseums.Org>
2018-02-09 15:51:30 -0500

    Handle missed alarms with a notification that will prompt user to open app to reschedule alarms

--- android/src/main/java/com/dawnchorus/react-native-android-alarms/RebootService.java
+++ android/src/main/java/com/dawnchorus/react-native-android-alarms/RebootService.java
@@ -1,8 +1,11 @@
 package com.dawnchorus.alarms;
 
 import android.app.IntentService;
+import 	android.content.res.Resources;
+import android.graphics.drawable.Drawable;
 
 import android.content.SharedPreferences;
+import 	android.support.v4.content.res.ResourcesCompat;
 
 import android.app.AlarmManager;
 import android.app.PendingIntent;
@@ -16,6 +19,17 @@ import android.util.Log;
 
 import java.util.HashMap;
 import java.util.Map;
+import java.lang.String;
+
+import android.graphics.BitmapFactory;
+import android.graphics.Bitmap;
+
+import android.app.Notification;
+import android.app.NotificationManager;
+import android.support.v4.app.NotificationCompat;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class RebootService extends IntentService {
 
@@ -25,22 +39,66 @@ public class RebootService extends IntentService {
 
     @Override
     protected void onHandleIntent(Intent intent) {
-
         // Your code to reset alarms.
         // All of these will be done in Background and will not affect
         // on phone's performance
         // Put the alarm into the preferences
 	    SharedPreferences alarms = getApplicationContext().getSharedPreferences("Alarms", 0);
 	    Map<String,?> keys = alarms.getAll();
-
+	    // See if we missed an alarm
+	    // If we did, we will launch app which will re-schedule alarms
+	    boolean missedAlarm = false;
+	    String missedAlarms = "";
 	    for(Map.Entry<String,?> entry : keys.entrySet()){ 
             String id = entry.getKey();  
             long timestamp = alarms.getLong(entry.getKey(), 0);
-            if (timestamp != 0) {
-            	setAlarmOnReboot(entry.getKey(), timestamp, false);
-            }
+           	long currentTime = System.currentTimeMillis();
+           	if (timestamp != 0 && currentTime - timestamp >= 0) {
+           		missedAlarm = true;
+           		missedAlarms += ("," + id + ",");
+           	} else {
+           		if (timestamp != 0) setAlarmOnReboot(entry.getKey(), timestamp, false);
+           	}
  		}
+
+ 		// If we did not miss an alarm, re schedule upcoming alarms in the background
+ 		if (missedAlarm) {
+ 			notifyUserOfMissedAlarm(getApplicationContext(), missedAlarms);
+ 		} 
     }
+
+    private void notifyUserOfMissedAlarm(Context context, String missedAlarms) {
+      Resources res = context.getResources();
+      String packageName = context.getApplicationContext().getPackageName();
+      Intent launchIntent = context.getPackageManager().getLaunchIntentForPackage(packageName);
+
+      String smallIconName = "ic_alarm";
+      int smallIconResId = res.getIdentifier(smallIconName, "drawable", packageName);
+
+      String largeIconName = "notification_icon";
+      int largeIconResId = res.getIdentifier(largeIconName, "drawable", packageName);
+      Bitmap largeIconBitmap = BitmapFactory.decodeResource(res, largeIconResId);
+
+      NotificationCompat.Builder builder =
+      new NotificationCompat.Builder(this)
+      .setSmallIcon(smallIconResId)
+      .setLargeIcon(largeIconBitmap)
+      .setContentTitle("You missed an alarm!")
+      .setContentText("Tap to re-schedule.")
+      .setAutoCancel(true)
+      .setPriority(1)
+      .setDefaults(Notification.DEFAULT_VIBRATE);
+
+      launchIntent.putExtra("missedAlarms", missedAlarms);
+      
+      PendingIntent contentIntent = PendingIntent.getActivity(this, 0, launchIntent,
+         PendingIntent.FLAG_UPDATE_CURRENT);
+      builder.setContentIntent(contentIntent);
+
+      // Add as notification
+      NotificationManager manager = (NotificationManager) getSystemService(Context.NOTIFICATION_SERVICE);
+      manager.notify(0, builder.build());
+   }
 
     public final void setAlarmOnReboot(String id, long timestamp, boolean inexact) {
 	    PendingIntent pendingIntent = createPendingIntent(id);
@@ -48,7 +106,6 @@ public class RebootService extends IntentService {
 	    // get the alarm manager, and schedule an alarm that calls the receiver
 	    // We will use setAlarmClock because we want an indicator to show in the status bar.
 	    // If you want to modify it and are unsure what to method to use, check https://plus.google.com/+AndroidDevelopers/posts/GdNrQciPwqo
-	    
 	    if(!inexact) {
 	//      if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M)
 	//        getAlarmManager().setExactAndAllowWhileIdle(AlarmManager.RTC_WAKEUP, timestampLong, pendingIntent);
@@ -62,6 +119,20 @@ public class RebootService extends IntentService {
 	      getAlarmManager().set(AlarmManager.RTC_WAKEUP, timestampLong, pendingIntent);
 	    }
 	  }
+
+	 private void launchApplicationBecauseMissedAlarm(Context context, String missedAlarms) {
+        String packageName = context.getApplicationContext().getPackageName();
+        Intent launchIntent = context.getPackageManager().getLaunchIntentForPackage(packageName);
+
+        launchIntent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+        launchIntent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK);
+        launchIntent.addFlags(Intent.FLAG_ACTIVITY_SINGLE_TOP);
+        launchIntent.putExtra("missedAlarms", missedAlarms);
+
+        context.startActivity(launchIntent);
+
+        Log.i("ReactNativeAppLauncher", "AlarmReceiver: Launching: " + packageName + "missedAlarms " + missedAlarms);
+    }     
 
 	private PendingIntent createPendingIntent(String id) {
 	    Context context = getApplicationContext();
